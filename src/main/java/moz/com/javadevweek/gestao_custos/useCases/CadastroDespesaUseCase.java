624ffdf29e3feabd7db0a5189a36c33491bef810
Shelton <sheltonjose02@gmail.com>
2026-01-24 01:56:06 +0200

    Update CadastroDespesaUseCase.java

--- src/main/java/moz/com/javadevweek/gestao_custos/useCases/CadastroDespesaUseCase.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/useCases/CadastroDespesaUseCase.java
@@ -1,6 +1,14 @@
 package moz.com.javadevweek.gestao_custos.useCases;
 
+import moz.com.javadevweek.gestao_custos.entity.Despesa;
+
 public class CadastroDespesaUseCase {
-    
+   //SOLID
+   //Single Responsibility Principle
+
+   public void execute(Despesa despesa){
+      System.out.println(despesa);
+       
+   }
 
 }
