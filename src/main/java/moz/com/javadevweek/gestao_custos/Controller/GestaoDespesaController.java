9481eb00abecbaa93a0df466bcd7cf9e85283e5b
Shelton <sheltonjose02@gmail.com>
2026-01-24 01:11:09 +0200

    Update GestaoDespesaController.java

--- src/main/java/moz/com/javadevweek/gestao_custos/Controller/GestaoDespesaController.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/Controller/GestaoDespesaController.java
@@ -1,5 +1,7 @@
 package moz.com.javadevweek.gestao_custos.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -7,7 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/gestao")
 public class GestaoDespesaController {
+
+
+
+    @PostMapping("/create")
+    public void createDespesa(){
+
+
+
+    }
+
+
+
 
 
 }
