fa417e27ef376e70c99d7c7fa7f8608c7b7f714a
Shelton <sheltonjose02@gmail.com>
2026-01-24 01:33:39 +0200

    Update Despesa.java

--- src/main/java/moz/com/javadevweek/gestao_custos/entity/Despesa.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/entity/Despesa.java
@@ -2,15 +2,31 @@ package moz.com.javadevweek.gestao_custos.entity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "despesas")
 public class Despesa {
 
-    
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
     private String descricao;
     private LocalDate data;
     private BigDecimal valor;
     private String categoria;
+    
+    @CreatedDate
     private LocalDate dataCriacao;
     private String email;
 
