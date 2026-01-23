c6d6b5c35fc9868fa3bc150e26deeccaa7a874ac
Shelton <sheltonjose02@gmail.com>
2026-01-24 01:04:18 +0200

    Update GestaoDespesaController.java

--- src/main/java/moz/com/javadevweek/gestao_custos/Controller/GestaoDespesaController.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/Controller/GestaoDespesaController.java
@@ -1,5 +1,13 @@
 package moz.com.javadevweek.gestao_custos.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+@RestController
 public class GestaoDespesaController {
+
 
 }
