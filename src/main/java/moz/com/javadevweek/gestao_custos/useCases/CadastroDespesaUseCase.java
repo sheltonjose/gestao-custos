ee9ed3805984d976c04ad316801f5d68e0dab1a0
Shelton <sheltonjose02@gmail.com>
2026-01-31 14:12:56 +0200

    Update CadastroDespesaUseCase.java

--- src/main/java/moz/com/javadevweek/gestao_custos/useCases/CadastroDespesaUseCase.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/useCases/CadastroDespesaUseCase.java
@@ -1,8 +1,10 @@
 package moz.com.javadevweek.gestao_custos.useCases;
 
+import org.springframework.stereotype.Service;
+
 import moz.com.javadevweek.gestao_custos.entity.Despesa;
 
-
+@Service
 public class CadastroDespesaUseCase {
    //SOLID
    //Single Responsibility Principle
