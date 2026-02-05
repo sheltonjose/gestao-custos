f1ae37f2740a7f7dcd29ccfec33edacfb762aa35
Shelton <sheltonjose02@gmail.com>
2026-02-05 14:05:40 +0200

    Update CadastroDespesaUseCase.java

--- src/main/java/moz/com/javadevweek/gestao_custos/useCases/CadastroDespesaUseCase.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/useCases/CadastroDespesaUseCase.java
@@ -2,8 +2,8 @@ package moz.com.javadevweek.gestao_custos.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import moz.com.javadevweek.gestao_custos.entity.Despesa;
+import moz.com.javadevweek.gestao_custos.repository.DespesaRepository;
 
 @Service
 public class CadastroDespesaUseCase {
@@ -11,12 +11,23 @@ public class CadastroDespesaUseCase {
    //Single Responsibility Principle
 
    @Autowired
-   
+   private DespesaRepository despesaRepository;
 
-   public void execute(Despesa despesa){
-    
 
-       
+   public Despesa execute(Despesa despesa){
+
+      if(despesa.getCategoria()==null || despesa.getDescricao()==null || despesa.getEmail()== null || despesa.getValor() == null ){
+
+
+      }
+
+
+      despesa = despesaRepository.save(despesa);
+
+      return despesa;
+
+      
+      
    }
 
 }
