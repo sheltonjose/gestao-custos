79536c3004f4154d105adefca833ee3c0d2132b3
Shelton <sheltonjose02@gmail.com>
2026-02-06 23:15:43 +0200

    Create BuscaDespesaUseCase.java

---
+++ src/main/java/moz/com/javadevweek/gestao_custos/useCases/BuscaDespesaUseCase.java
@@ -0,0 +1,26 @@
+package moz.com.javadevweek.gestao_custos.useCases;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import moz.com.javadevweek.gestao_custos.entity.Despesa;
+import moz.com.javadevweek.gestao_custos.repository.DespesaRepository;
+
+@Service
+public class BuscaDespesaUseCase {
+
+
+@Autowired
+private DespesaRepository despesaRepository;
+
+public List<Despesa> buscarPorEmailEData(String email, LocalDate data){
+    despesaRepository.findByEmail(email);
+
+
+
+}
+
+}
