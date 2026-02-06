436b0295c2bb4242fa8f409d86506d7de99f7662
Shelton <sheltonjose02@gmail.com>
2026-02-06 23:15:48 +0200

    Update DespesaRepository.java

--- src/main/java/moz/com/javadevweek/gestao_custos/repository/DespesaRepository.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/repository/DespesaRepository.java
@@ -1,6 +1,8 @@
 package moz.com.javadevweek.gestao_custos.repository;
 
+import java.util.List;
 import java.util.UUID;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +10,9 @@ import moz.com.javadevweek.gestao_custos.entity.Despesa;
 
 public interface DespesaRepository extends JpaRepository<Despesa, UUID> {
 
-    
+    List<Despesa> findByEmail(String email);
+
+
+
+
 } 
