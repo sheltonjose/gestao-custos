88e9b912077e0d2be3223223527c1d6346ebc238
Shelton <sheltonjose02@gmail.com>
2026-02-07 00:41:21 +0200

    Update GestaoDeDespesaSeeder.java

--- src/main/java/moz/com/javadevweek/gestao_custos/performance/GestaoDeDespesaSeeder.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/performance/GestaoDeDespesaSeeder.java
@@ -1,6 +1,16 @@
 package moz.com.javadevweek.gestao_custos.performance;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-public class GestaoDeDespesaSeeder {
+
+@Component
+public class GestaoDeDespesaSeeder implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Lógica para popular a base de dados com dados de teste, se necessário
+        System.out.println("Aplicação iniciada. Base de dados pronta para uso.");
+    }
 
 }
