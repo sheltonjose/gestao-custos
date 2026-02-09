b453e7de594a45b06146aa8d44950d3a09615169
Shelton <sheltonjose02@gmail.com>
2026-02-09 14:22:13 +0200

    Update GestaoDespesaPerformance.java

--- src/main/java/moz/com/javadevweek/gestao_custos/performance/GestaoDespesaPerformance.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/performance/GestaoDespesaPerformance.java
@@ -1,8 +1,12 @@
 package moz.com.javadevweek.gestao_custos.performance;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import moz.com.javadevweek.gestao_custos.entity.Despesa;
 import moz.com.javadevweek.gestao_custos.repository.DespesaRepository;
 
 
@@ -13,13 +17,21 @@ public class GestaoDespesaPerformance {
 
 
     @Autowired
-    private DespesaRepository despesaRepository;
+     DespesaRepository repository;
+
+
+    public ResponseEntity<List<Despesa>> ListarSemPaginacao(){
+        long start = System.currentTimeMillis();
+        var despesas = repository.findAll();
+
+        
+        return ResponseEntity.ok(despesas);
+    }
 
 
 
 
 
 
-    
 
 }
