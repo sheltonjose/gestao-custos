27d1c41f3a14482720a4fd09a7dca4f12f61431a
Shelton <sheltonjose02@gmail.com>
2026-02-09 13:43:53 +0200

    Update GestaoDespesaPerformance.java

--- src/main/java/moz/com/javadevweek/gestao_custos/performance/GestaoDespesaPerformance.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/performance/GestaoDespesaPerformance.java
@@ -1,8 +1,25 @@
 package moz.com.javadevweek.gestao_custos.performance;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import moz.com.javadevweek.gestao_custos.repository.DespesaRepository;
+
+
 
 @RequestMapping("/gestao/performance")
 public class GestaoDespesaPerformance {
+
+
+
+    @Autowired
+    private DespesaRepository despesaRepository;
+
+
+
+
+
+
+    
 
 }
