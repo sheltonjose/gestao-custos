bdc00a6f3f68a40caa70fd645f83c494a2543aac
Shelton <sheltonjose02@gmail.com>
2026-02-09 13:42:08 +0200

    Create GestaoDespesaPerformance.java

---
+++ src/main/java/moz/com/javadevweek/gestao_custos/performance/GestaoDespesaPerformance.java
@@ -0,0 +1,8 @@
+package moz.com.javadevweek.gestao_custos.performance;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/gestao/performance")
+public class GestaoDespesaPerformance {
+
+}
