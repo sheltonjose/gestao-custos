5bbaf0d9554ae1e9411fc3e7fc542a7feeb41593
Shelton <sheltonjose02@gmail.com>
2026-02-10 15:08:54 +0200

    Update DespesaRepository.java

--- src/main/java/moz/com/javadevweek/gestao_custos/repository/DespesaRepository.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/repository/DespesaRepository.java
@@ -4,7 +4,8 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import moz.com.javadevweek.gestao_custos.entity.Despesa;
@@ -14,6 +15,8 @@ public interface DespesaRepository extends JpaRepository<Despesa, UUID> {
     List<Despesa> findByEmail(String email);
     List<Despesa> findByEmailAndData(String email, LocalDate data);
     
+    Page<Despesa> findByEmail(String email,Pageable pageable);
+
 
 
 
