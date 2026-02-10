9e50cfa5a704368ca773e61bb7bc1415873d6f61
Shelton <sheltonjose02@gmail.com>
2026-02-10 15:38:56 +0200

    Update GestaoDeCustosPesssoaisApplication.java

--- src/main/java/moz/com/javadevweek/gestao_custos/GestaoDeCustosPesssoaisApplication.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/GestaoDeCustosPesssoaisApplication.java
@@ -2,7 +2,7 @@ package moz.com.javadevweek.gestao_custos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
 
