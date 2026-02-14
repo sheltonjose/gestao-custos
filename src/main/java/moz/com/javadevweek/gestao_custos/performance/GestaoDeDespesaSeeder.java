9c6a4a4da76df3e9d0caf2094ce4ecee7fcb27e7
Shelton <sheltonjose02@gmail.com>
2026-02-14 21:30:48 +0200

    Update GestaoDeDespesaSeeder.java

--- src/main/java/moz/com/javadevweek/gestao_custos/performance/GestaoDeDespesaSeeder.java
+++ src/main/java/moz/com/javadevweek/gestao_custos/performance/GestaoDeDespesaSeeder.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Component;
 
 import moz.com.javadevweek.gestao_custos.entity.Despesa;
 import moz.com.javadevweek.gestao_custos.repository.DespesaRepository;
